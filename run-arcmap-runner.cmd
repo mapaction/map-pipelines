d3b88aa37a9fe9f6a428d64687365575905f0c33
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2020-05-16 12:32:06 +0100

    tweak to map creation script

--- run-arcmap-runner.cmd
+++ run-arcmap-runner.cmd
@@ -5,6 +5,10 @@ set script_root=%~dp0
 set script_root=%script_root:~0,-1%
 ::call %root%\build-fresh-data.cmd
 echo %script_root%
+
+:: Use this as a test of whether or not we are running on a local machine or on GoCD
+if not defined py set opidlist=2019lka01 2019mli01 2019slv01
+if not defined py set outputmaps=%script_root%\outputmaps
 if not defined py set py=D:\MapAction\ve-with-arcmap2\Scripts\python.exe 
 ::set py=C:/py27arcgis106/ArcGIS10.6/python.exe
 
@@ -15,6 +19,8 @@ if not defined tempoutputs set tempoutputs=%script_root%\tempoutputs
 if not exist %tempoutputs% mkdir %tempoutputs%
 if not defined outputmaps set outputmaps=%script_root%\..\outputmaps
 if not exist %outputmaps% mkdir %outputmaps%
+if not defined dest_root set dest_root=%script_root%\output-cmfs
+
 
 set "productlist="Country Overview with Admin 1 Boundaries and Topography", "Atlas Admin 1 Boundaries and P-Codes plus Admin 2 Boundaries""
 
@@ -46,17 +52,12 @@ for %%G in (%opidlist%) do (
 		)
 
 	rem Ugly hack to extract maps from CMFs whilst certain tools which won't deal with UNC paths
-	robocopy %dest_root%\%%G\GIS\3_Mapping\34_Map_Products_MapAction %tempoutputs% *.jpg *.zip /mir
+	robocopy %dest_root%\%%G\GIS\3_Mapping\34_Map_Products_MapAction %tempoutputs% *.jpg *.zip /mir > nul
 	if not exist %outputmaps%\%%G mkdir %outputmaps%\%%G
-	forfiles -p %tempoutputs% -s -m *.* /c "cmd /c copy @path %outputmaps%\%%G\@file /y"
+	forfiles -p %tempoutputs% -s -m *.* /c "cmd /c copy @path %outputmaps%\%%G\@file /y" > nul
+	echo have probably just copied some files
 )
 
-
-
-::"Country Overview with Admin 1 Boundaries and Topography"
-::"Atlas Admin 1 Boundaries & P-Codes plus Admin 2 Boundaries"
-::--template    "%root%\%%G\GIS\3_Mapping\32_Map_Templates\arcmap-10.6_reference_portrait_bottom.mxd" ^
-
-%py% -m coverage html --include="D:/code/github/mapactionpy*"  --directory=%root%\htmlcov
+%py% -m coverage html --include="D:/code/github/mapactionpy*"  --directory=%script_root%\htmlcov
 
 
